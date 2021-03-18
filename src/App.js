37123a9786f7d49a1bea8e25e6b8a198ba363d3f
Your Name <captainravyar@gmail.com>
2021-03-18 23:33:21 +0300

    SmallStyleChanges

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import HeroSection from "./components/HeroSection/HeroSection"
+
 function App() {
 	return (
 		<div>
--- src/components/HeroSection/style.jsx
+++ src/components/HeroSection/style.jsx
@@ -5,19 +5,30 @@ const useStyles = makeStyles({
 		backgroundColor: "#3AAFA9"
 	},
 	fontTitleSize: {
-		fontSize: "1.3rem",
-		fontWeight: "bold"
+		fontSize: "2.5rem",
+		fontWeight: "bold",
+		marginTop:"160px",
+		marginRight:"20px",
+		textAlign:'center'
 	},
 	txtFontSize: {
-		fontSize: "1rem"
+		fontSize: "1.2rem",
+		marginTop:"50px",
+		marginRight:"60px",
+		marginLeft:"10px",
+		textAlign:'center'
 	},
 	img: {
 		background: "white",
-		borderRadius: "10px"
+		borderRadius: "10px",
+		marginTop:"150px",
+		marginBottom:"80px"
 	},
 	btn: {
 		color: "white !important",
-		borderRadius: "25px !important"
+		borderRadius: "25px !important",
+		marginTop:"50px",
+		marginLeft:"20px"
 	},
 	lostBtn: {
 		background: "#2B7A78 !important"
