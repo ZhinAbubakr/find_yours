61fbdc6bba5cd1c3c9b8c785b0568b50b96c8975
Your Name <captainravyar@gmail.com>
2021-03-23 01:30:44 +0300

    add InfoSection

--- src/components/InfoSection/InfoSection.jsx
+++ src/components/InfoSection/InfoSection.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid';
+import useStyles from './style'
+import image1 from '../../images/1.svg'
+import image2 from '../../images/2.svg'
+import image3 from '../../images/3.svg'
+
+
+
 
 const InfoSection = () => {
+	const classes = useStyles()
+
     return (
-        <div>
-            !!
+        <div className={classes.root}>
+         <Grid container spacing={10} >
+                <Grid item xs={12} lg={4}>
+                        <img src={image1} className={classes.image}/>
+                        <h3 className={classes.title}>Lorem ipsum dolor sit amet.</h3>
+                        <p className={classes.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel odio nisi. Quisque a congue ante. Maecenas non dolor pretium.</p>
+                </Grid>
+                <Grid item xs={12} lg={4}>
+                <img src={image2} className={classes.image}/>
+                <h3 className={classes.title}>Lorem ipsum dolor sit amet,</h3>
+                        <p className={classes.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel odio nisi. Quisque a congue ante. Maecenas non dolor pretium.</p>                
+                </Grid>
+                <Grid item xs={12} lg={4}>
+                <img src={image3} className={classes.image}/>
+
+                <h3 className={classes.title}>Lorem ipsum dolor sit amet.</h3>
+                        <p className={classes.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel odio nisi. Quisque a congue ante. Maecenas non dolor pretium.</p>                
+                </Grid>
+         </Grid>
         </div>
     )
 }
