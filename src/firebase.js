f0820752fe5bb6d235750adda778e03a68effd27
Othman Shareef <othmanosx@gmail.com>
2021-03-23 20:35:43 +0300

    Refactor: remove comments closes #26

--- src/firebase.js
+++ src/firebase.js
@@ -2,8 +2,6 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/analytics"
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
     apiKey: "AIzaSyDaQIgxU_BSwBeokkFcN4tIVrKkkz57YWY",
     authDomain: "findyours-eb195.firebaseapp.com",
@@ -13,7 +11,6 @@ import "firebase/analytics"
     appId: "1:98252038188:web:6e67a29b51bb59e5f07189",
     measurementId: "G-T019N9EWTW"
   };
-  // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
 
