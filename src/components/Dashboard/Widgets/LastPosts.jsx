4f7186e9c40a8d7a3121b50da8a83ba70c1e4c45
Othman Shareef <othmanosx@gmail.com>
2021-03-24 15:54:10 +0300

    Feat: create mylastposts inner component

--- src/components/Dashboard/Widgets/LastPosts.jsx
+++ src/components/Dashboard/Widgets/LastPosts.jsx
@@ -2,10 +2,57 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
-import Avatar from "@material-ui/core/Avatar";
+// import Avatar from "@material-ui/core/Avatar";
 import { grey } from "@material-ui/core/colors";
-import Button from "@material-ui/core/Button";
-import ghost from "../../../Assets/Ghost.jpg";
+// import ghost from "../../../Assets/Ghost.jpg";
+import CardMedia from "@material-ui/core/CardMedia";
+
+import { withStyles } from "@material-ui/core/styles";
+import MuiAccordion from "@material-ui/core/Accordion";
+import MuiAccordionSummary from "@material-ui/core/AccordionSummary";
+import MuiAccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+
+const Accordion = withStyles({
+  root: {
+    border: "1px solid rgba(0, 0, 0, .125)",
+    boxShadow: "none",
+    "&:not(:last-child)": {
+      borderBottom: 0,
+    },
+    "&:before": {
+      display: "none",
+    },
+    "&$expanded": {
+      margin: "auto",
+    },
+  },
+  expanded: {},
+})(MuiAccordion);
+
+const AccordionSummary = withStyles({
+  root: {
+    backgroundColor: "rgba(0, 0, 0, .03)",
+    borderBottom: "1px solid rgba(0, 0, 0, .125)",
+    marginBottom: -1,
+    minHeight: 56,
+    "&$expanded": {
+      minHeight: 56,
+    },
+  },
+  content: {
+    "&$expanded": {
+      margin: "12px 0",
+    },
+  },
+  expanded: {},
+})(MuiAccordionSummary);
+
+const AccordionDetails = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiAccordionDetails);
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -39,29 +86,86 @@ const useStyles = makeStyles(() => ({
     color: "white",
     height: "15px",
   },
+  media: {
+    height: "0%",
+    paddingTop: "56.25%", // 16:9
+    marginTop: "0px",
+    marginBottom: "10px",
+    borderRadius: "10px",
+  },
 }));
 
 export default function LastPosts() {
   const classes = useStyles();
+  const [expanded, setExpanded] = React.useState("panel1");
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
 
   return (
     <Card className={classes.root}>
       <CardHeader
-        avatar={<Avatar src={ghost} className={classes.avatar} />}
-        title="Othman"
+        // avatar={<Avatar src={ghost} className={classes.avatar} />}
+        title="My Last Posts"
         className={classes.header}
       />
-      <div className={classes.buttons}>
-        <Button variant="contained" className={classes.button} disableElevation>
-          Profile
-        </Button>
-        <Button variant="contained" className={classes.button} disableElevation>
-          favorite
-        </Button>
-        <Button variant="contained" className={classes.button} disableElevation>
-          my posts
-        </Button>
-      </div>
+      <Accordion
+        square
+        expanded={expanded === "panel1"}
+        onChange={handleChange("panel1")}
+      >
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+          <Typography>My lost cat</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <CardMedia
+              className={classes.media}
+              image="https://cosmosmagazine.com/wp-content/uploads/2020/02/190404-cat-full.jpg"
+              title="Paella dish"
+            />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        square
+        expanded={expanded === "panel2"}
+        onChange={handleChange("panel2")}
+      >
+        <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+          <Typography>My lost wallet</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        square
+        expanded={expanded === "panel3"}
+        onChange={handleChange("panel3")}
+      >
+        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+          <Typography>My lost life</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
     </Card>
   );
 }
