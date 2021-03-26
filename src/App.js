1ffee89ba0e6c0e7b1c0a60454b36af81934c5ec
zhin <zheen99abubakr@gmail.com>
2021-03-26 17:16:35 +0300

    Refactor: remove components from app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import Profile from "./components/Profile/Profile"
-import Navbar from "./components/Navbar/Navbar"
 
 function App() {
 	return (
 		<div>
-			<Navbar />
-			<Profile />
+
 		</div>
 	)
 }
