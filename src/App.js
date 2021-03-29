db66da586ac7de4a1c0af88441c310c9b7e1b285
JalalArif <jalal.00066564@gmail.com>
2021-03-29 09:11:22 -0800

    Create privacy policy page Fixes #23

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import PrivacyPolicy from './components/PrivacyPolicy/privacyPolicy'
-import Footer from './components/Footer/Footer'
+
 export default function App() {
 	return (
 		<div>
-			<Navbar/>
-			< PrivacyPolicy />
-			<Footer/>
+		
 		</div>
 	)
 }
