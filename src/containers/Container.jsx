1f098dc02101b7c6080a8dd5a870a0a74319bd53
Othman Shareef <othmanosx@gmail.com>
2021-04-06 21:22:18 +0300

    Feat: show posts from firebase

--- src/containers/Container.jsx
+++ src/containers/Container.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
-import Footer from '../components/Footer/Footer'
-import LandingPage from './LandingPage'
-import Dashboard from '../components/Dashboard/Dashboard'
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import LandingPage from "./LandingPage";
+import Dashboard from "../components/Dashboard/Dashboard";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import PrivacyPolicy from '../components/PrivacyPolicy/privacyPolicy'
-import About from '../components/AboutUs/AboutUs'
-import {ProfileProvider} from '../profileContext'
-
+import PrivacyPolicy from "../components/PrivacyPolicy/privacyPolicy";
+import About from "../components/AboutUs/AboutUs";
+import { ProfileProvider } from "../profileContext";
+import Contact from "./CountactUs";
+import Profile from "../components/Profile/Profile";
 
 export default function Container() {
-    return (
-        <div>
-            <BrowserRouter>
-            <ProfileProvider>
-                <Navbar />
-                    <Switch>
-                    <Route exact path="/" component={LandingPage} />
-                    <Route path="/dashboard" component={Dashboard} />
-                    <Route path="/PrivacyPolicy" component={PrivacyPolicy} />
-                    <Route path="/about" component={About} />
-                    <Route path="/:404" component={null} />
-                    </Switch>
-                <Footer />
-            </ProfileProvider>
-            </BrowserRouter>
-        </div>
-    )
+  return (
+    <div>
+      <BrowserRouter>
+        <ProfileProvider>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/PrivacyPolicy" component={PrivacyPolicy} />
+            <Route path="/about" component={About} />
+            <Route path="/contactUs" component={Contact} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/:404" component={null} />
+          </Switch>
+          <Footer />
+        </ProfileProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
