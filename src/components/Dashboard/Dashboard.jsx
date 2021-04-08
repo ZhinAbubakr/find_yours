012ca91fd96f45435966a3c4453898f3a691c50a
Murtaja Adnan <murtaja07805@gmail.com>
2021-04-08 20:49:38 +0300

    Feat: add the feat functionality

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,170 +1,168 @@
-import Navbar from "../Navbar/Navbar";
-import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
-import { Avatar } from "@material-ui/core";
-import "./styles.css";
-import Post from "./Post/Post";
+import Navbar from "../Navbar/Navbar"
+import Grid from "@material-ui/core/Grid"
+import Button from "@material-ui/core/Button"
+import { Avatar } from "@material-ui/core"
+import "./styles.css"
+import Post from "./Post/Post"
 
-import React, { useContext, useState } from "react";
-import Widget from "./Widgets/Widget";
-import LastPosts from "./Widgets/LastPosts";
-import Cities from "./Widgets/Cities";
-import Filters from "./Widgets/Filters";
-import SearchBar from "material-ui-search-bar";
+import React, { useContext, useState } from "react"
+import Widget from "./Widgets/Widget"
+import LastPosts from "./Widgets/LastPosts"
+import Cities from "./Widgets/Cities"
+import Filters from "./Widgets/Filters"
+import SearchBar from "material-ui-search-bar"
 // import LinearProgress from "@material-ui/core/LinearProgress";
-import { useStyleDashboard } from "./Widgets/Style";
-import FiltersFlat from "./Widgets/FiltersFlat";
-import { ProfileContext } from "../../profileContext";
-import { db } from "../../firebase";
-import CircularProgress from "@material-ui/core/CircularProgress";
+import { useStyleDashboard } from "./Widgets/Style"
+import FiltersFlat from "./Widgets/FiltersFlat"
+import { ProfileContext } from "../../profileContext"
+import { db } from "../../firebase"
+import CircularProgress from "@material-ui/core/CircularProgress"
 
 export default function Dashboard() {
-  const [profile] = useContext(ProfileContext);
-  const [posts, setPosts] = useState();
-  const [state, setState] = React.useState({
-    mobileView: false,
-    mediumView: false,
-  });
+	const [profile] = useContext(ProfileContext)
+	const [posts, setPosts] = useState()
+	const [state, setState] = React.useState({
+		mobileView: false,
+		mediumView: false
+	})
 
-  const { mobileView, mediumView } = state;
-  React.useEffect(() => {
-    db.collection("posts").onSnapshot((snapshot) => {
-      setPosts(snapshot.docs.map((doc) => doc.data()));
-    });
-  }, []);
-  React.useEffect(() => {
-    const setResponsiveness = () => {
-      if (window.innerWidth < 812) {
-        setState((prevState) => ({ ...prevState, mobileView: true }));
-      }
-      if (window.innerWidth < 1210) {
-        setState((prevState) => ({ ...prevState, mediumView: true }));
-      }
+	const { mobileView, mediumView } = state
+	React.useEffect(() => {
+		db.collection("posts").onSnapshot((snapshot) => {
+			setPosts(snapshot.docs.map((doc) => doc.data()))
+		})
+	}, [])
+	React.useEffect(() => {
+		const setResponsiveness = () => {
+			if (window.innerWidth < 812) {
+				setState((prevState) => ({ ...prevState, mobileView: true }))
+			}
+			if (window.innerWidth < 1210) {
+				setState((prevState) => ({ ...prevState, mediumView: true }))
+			}
 
-      // window.innerWidth < 900
-      //   ? setState((prevState) => ({ ...prevState, mobileView: true }))
-      //   : setState((prevState) => ({ ...prevState, mobileView: false }));
-    };
+			// window.innerWidth < 900
+			//   ? setState((prevState) => ({ ...prevState, mobileView: true }))
+			//   : setState((prevState) => ({ ...prevState, mobileView: false }));
+		}
 
-    setResponsiveness();
+		setResponsiveness()
 
-    window.addEventListener("resize", () => setResponsiveness());
-  }, []);
+		window.addEventListener("resize", () => setResponsiveness())
+	}, [])
 
-  const { container, searchfield, button, widget } = useStyleDashboard();
-  return (
-    <div>
-      <Navbar />
+	const { container, searchfield, button, widget } = useStyleDashboard()
+	return (
+		<div>
+			<Navbar />
 
-      {mediumView && (
-        <Grid container justify="center" className={container}>
-          <Grid item xs={12} sm={8} lg={8}>
-            <div style={{ margin: "0 20px" }}>
-              <div className="messageSender">
-                <div className="messageSender_top">
-                  <Avatar src={profile.imageUrl} />
-                  <div className="searchbar">
-                    <SearchBar
-                      className={searchfield}
-                      // Resource: https://codesandbox.io/s/mz7nx9v02j?file=/src/appStore.js
-                      // onRequestSearch={fetch}
-                      placeholder="Search items ..."
-                      autoFocus
-                    />
-                    {/* {isLoading && <LinearProgress />} */}
-                  </div>
-                  <div className="buttons">
-                    <Button
-                      {...{
-                        className: button,
-                      }}
-                      variant="contained"
-                      color="primary"
-                      disableElevation
-                    >
-                      Post
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {mobileView && <FiltersFlat />}
-            <div className="posts">
-              {posts ? (
-                posts.map((post) => <Post post={post} />)
-              ) : (
-                <CircularProgress />
-              )}
-            </div>
-          </Grid>
+			{mediumView && (
+				<Grid container justify="center" className={container}>
+					<Grid item xs={12} sm={8} lg={8}>
+						<div style={{ margin: "0 20px" }}>
+							<div className="messageSender">
+								<div className="messageSender_top">
+									<Avatar src={profile.imageUrl} />
+									<div className="searchbar">
+										<SearchBar
+											className={searchfield}
+											// Resource: https://codesandbox.io/s/mz7nx9v02j?file=/src/appStore.js
+											// onRequestSearch={fetch}
+											placeholder="Search items ..."
+											autoFocus
+										/>
+										{/* {isLoading && <LinearProgress />} */}
+									</div>
+									<div className="buttons">
+										<Button
+											{...{
+												className: button
+											}}
+											variant="contained"
+											color="primary"
+											disableElevation>
+											Post
+										</Button>
+									</div>
+								</div>
+							</div>
+						</div>
+						{mobileView && <FiltersFlat />}
+						<div className="posts">
+							{posts ? (
+								posts.map((post) => <Post post={post} />)
+							) : (
+								<CircularProgress />
+							)}
+						</div>
+					</Grid>
 
-          {!mobileView && (
-            <Grid item sm={4} lg={4} className={widget}>
-              <div className="widget">
-                <Filters />
-                <Cities />
-              </div>
-            </Grid>
-          )}
-        </Grid>
-      )}
+					{!mobileView && (
+						<Grid item sm={4} lg={4} className={widget}>
+							<div className="widget">
+								<Filters />
+								<Cities />
+							</div>
+						</Grid>
+					)}
+				</Grid>
+			)}
 
-      {!mediumView && (
-        <Grid container justify="center" className={container}>
-          <Grid item sm={3} lg={3} className={widget}>
-            <div className="widget">
-              <div>
-                <Widget />
-                <LastPosts />
-              </div>
-            </div>
-          </Grid>
-          <Grid item sm={6} lg={6}>
-            <div style={{ margin: "0 20px" }}>
-              <div className="messageSender">
-                <div className="messageSender_top">
-                  <Avatar src={profile.imageUrl} />
-                  <div className="searchbar">
-                    <SearchBar
-                      className={searchfield}
-                      // Resource: https://codesandbox.io/s/mz7nx9v02j?file=/src/appStore.js
-                      // onRequestSearch={fetch}
-                      placeholder="Search items ..."
-                      autoFocus
-                    />
-                    {/* {isLoading && <LinearProgress />} */}
-                  </div>
-                  <div className="buttons">
-                    <Button
-                      {...{
-                        className: button,
-                      }}
-                      variant="contained"
-                      color="primary"
-                      disableElevation
-                    >
-                      Submit item
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="posts">
-              {posts ? (
-                posts.map((post) => <Post post={post} />)
-              ) : (
-                <CircularProgress />
-              )}
-            </div>
-          </Grid>
-          <Grid item sm={3} lg={3} className={widget}>
-            <div className="widget">
-              <Filters />
-              <Cities />
-            </div>
-          </Grid>
-        </Grid>
-      )}
-    </div>
-  );
+			{!mediumView && (
+				<Grid container justify="center" className={container}>
+					<Grid item sm={3} lg={3} className={widget}>
+						<div className="widget">
+							<div>
+								<Widget />
+								<LastPosts />
+							</div>
+						</div>
+					</Grid>
+					<Grid item sm={6} lg={6}>
+						<div style={{ margin: "0 20px" }}>
+							<div className="messageSender">
+								<div className="messageSender_top">
+									<Avatar src={profile.imageUrl} />
+									<div className="searchbar">
+										<SearchBar
+											className={searchfield}
+											// Resource: https://codesandbox.io/s/mz7nx9v02j?file=/src/appStore.js
+											// onRequestSearch={fetch}
+											placeholder="Search items ..."
+											autoFocus
+										/>
+										{/* {isLoading && <LinearProgress />} */}
+									</div>
+									<div className="buttons">
+										<Button
+											{...{
+												className: button
+											}}
+											variant="contained"
+											color="primary"
+											disableElevation>
+											Submit item
+										</Button>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="posts">
+							{posts ? (
+								posts.map((post) => <Post post={post} />)
+							) : (
+								<CircularProgress />
+							)}
+						</div>
+					</Grid>
+					<Grid item sm={3} lg={3} className={widget}>
+						<div className="widget">
+							<Filters />
+							<Cities />
+						</div>
+					</Grid>
+				</Grid>
+			)}
+		</div>
+	)
 }
--- src/components/Dashboard/Widgets/Filters.jsx
+++ src/components/Dashboard/Widgets/Filters.jsx
@@ -1,111 +1,139 @@
-import React from "react";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
+import React, { useEffect } from "react"
+import Card from "@material-ui/core/Card"
+import CardHeader from "@material-ui/core/CardHeader"
 // import Avatar from "@material-ui/core/Avatar";
 // import Button from "@material-ui/core/Button";
 // import ghost from "../../../Assets/Ghost.jpg";
-
-import InputLabel from "@material-ui/core/InputLabel";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
-import { useStylesFilter } from "./Style.js";
-
+import { db } from "../../../firebase"
+import InputLabel from "@material-ui/core/InputLabel"
+import FormControl from "@material-ui/core/FormControl"
+import Select from "@material-ui/core/Select"
+import { useStylesFilter } from "./Style.js"
 export default function Filters() {
-  const classes = useStylesFilter();
+	const classes = useStylesFilter()
 
-  const [state, setState] = React.useState({
-    age: "",
-    name: "hi",
-  });
+	const [state, setState] = React.useState({
+		// Status: false,
+		// "category ": "",
+		// color: "",
+		// city: ""
+	})
+	const [, setPosts] = React.useState([])
 
-  const handleChange = (event) => {
-    const name = event.target.name;
-    setState({
-      ...state,
-      [name]: event.target.value,
-    });
-  };
+	const handleChange = (event) => {
+		const name = event.target.name
+		setState({
+			...state,
+			[name]: event.target.value
+		})
+	}
 
-  return (
-    <Card className={classes.root}>
-      <CardHeader
-        // avatar={<Avatar src={ghost} className={classes.avatar} />}
-        title="Filters"
-        className={classes.fheader}
-      />
-      <div className={classes.fbuttons}>
-        <FormControl variant="outlined" className={classes.fformControl}>
-          <InputLabel htmlFor="outlined-age-native-simple">Catagory</InputLabel>
-          <Select
-            native
-            value={state.catagory}
-            onChange={handleChange}
-            label="Catagory"
-            inputProps={{
-              name: "catagory",
-              id: "outlined-age-native-simple",
-            }}
-          >
-            <option aria-label="None" value="" />
-            <option value={10}>Wallets</option>
-            <option value={20}>Documents</option>
-            <option value={30}>Other</option>
-          </Select>
-        </FormControl>
-        <FormControl variant="outlined" className={classes.fformControl}>
-          <InputLabel htmlFor="outlined-age-native-simple">Status</InputLabel>
-          <Select
-            native
-            value={state.catagory}
-            onChange={handleChange}
-            label="Status"
-            inputProps={{
-              name: "Status",
-              id: "outlined-age-native-simple",
-            }}
-          >
-            <option aria-label="None" value="" />
-            <option value={10}>Lost</option>
-            <option value={20}>Found</option>
-          </Select>
-        </FormControl>
-        <FormControl variant="outlined" className={classes.fformControl}>
-          <InputLabel htmlFor="outlined-age-native-simple">City</InputLabel>
-          <Select
-            native
-            value={state.city}
-            onChange={handleChange}
-            label="City"
-            inputProps={{
-              name: "city",
-              id: "outlined-age-native-simple",
-            }}
-          >
-            <option aria-label="None" value="" />
-            <option value={10}>Erbil</option>
-            <option value={20}>Baghdad</option>
-            <option value={30}>Basrah</option>
-          </Select>
-        </FormControl>
-        <FormControl variant="outlined" className={classes.fformControl}>
-          <InputLabel htmlFor="outlined-age-native-simple">Color</InputLabel>
-          <Select
-            native
-            value={state.color}
-            onChange={handleChange}
-            label="Color"
-            inputProps={{
-              name: "color",
-              id: "outlined-age-native-simple",
-            }}
-          >
-            <option aria-label="None" value="" />
-            <option value={10}>red</option>
-            <option value={20}>white</option>
-            <option value={30}>blue</option>
-          </Select>
-        </FormControl>
-      </div>
-    </Card>
-  );
+	useEffect(() => {
+		// to prevent the first render.
+		if (Object.keys(state).length != 0) {
+			handlePosts()
+		}
+	}, [state])
+
+	const handlePosts = () => {
+		const res = []
+
+		const query = db.collection("posts")
+		const category = state.category
+			? query.where("category ", "==", state["category"])
+			: query
+		const color = state.color
+			? category.where("color", "==", state["color"])
+			: category
+		const province = state.province
+			? color.where("province", "==", state["province"])
+			: color
+		const check = state["Status"] === "true" ? true : false
+		const isLost = state.Status
+			? province.where("isLost", "==", check)
+			: province
+
+		isLost.get().then((snapshot) => {
+			snapshot.docs.map((doc) => res.push(doc.data()))
+			setPosts(res)
+		})
+	}
+	return (
+		<Card className={classes.root}>
+			<CardHeader
+				// avatar={<Avatar src={ghost} className={classes.avatar} />}
+				title="Filters"
+				className={classes.fheader}
+			/>
+			<div className={classes.fbuttons}>
+				<FormControl variant="outlined" className={classes.fformControl}>
+					<InputLabel htmlFor="outlined-age-native-simple">Catagory</InputLabel>
+					<Select
+						native
+						value={state.category}
+						onChange={handleChange}
+						label="Category"
+						inputProps={{
+							name: "category",
+							id: "outlined-age-native-simple"
+						}}>
+						<option aria-label="None" value="" />
+						<option value="wallets">Wallets</option>
+						<option value="documents">Documents</option>
+						<option value="other">Other</option>
+					</Select>
+				</FormControl>
+				<FormControl variant="outlined" className={classes.fformControl}>
+					<InputLabel htmlFor="outlined-age-native-simple">Status</InputLabel>
+					<Select
+						native
+						value={state.Status}
+						onChange={handleChange}
+						label="Status"
+						inputProps={{
+							name: "Status",
+							id: "outlined-age-native-simple"
+						}}>
+						<option aria-label="None" value="" />
+						<option value={true}>Lost</option>
+						<option value={false}>Found</option>
+					</Select>
+				</FormControl>
+				<FormControl variant="outlined" className={classes.fformControl}>
+					<InputLabel htmlFor="outlined-age-native-simple">City</InputLabel>
+					<Select
+						native
+						value={state.province}
+						onChange={handleChange}
+						label="City"
+						inputProps={{
+							name: "province",
+							id: "outlined-age-native-simple"
+						}}>
+						<option aria-label="None" value="" />
+						<option value="erbil">Erbil</option>
+						<option value="baghdad">Baghdad</option>
+						<option value="basrah">Basrah</option>
+					</Select>
+				</FormControl>
+				<FormControl variant="outlined" className={classes.fformControl}>
+					<InputLabel htmlFor="outlined-age-native-simple">Color</InputLabel>
+					<Select
+						native
+						value={state.color}
+						onChange={handleChange}
+						label="Color"
+						inputProps={{
+							name: "color",
+							id: "outlined-age-native-simple"
+						}}>
+						<option aria-label="None" value="" />
+						<option value="red">red</option>
+						<option value="white">white</option>
+						<option value="blue">blue</option>
+					</Select>
+				</FormControl>
+			</div>
+		</Card>
+	)
 }
