2dd8337fc6ef0c6ea8fb27ba5ad1ca3cb6e248bd
zhin <zheen99abubakr@gmail.com>
2021-04-13 01:09:50 +0300

    Feat: add routes file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import React from 'react'
+import Cintainer from './containers/Container'
 
 export default function App() {
 	return (
 		<div>			
 			
-
+<Cintainer />
 			
 		</div>
 		
--- src/containers/Container.jsx
+++ src/containers/Container.jsx
@@ -7,7 +7,15 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import PrivacyPolicy from '../components/PrivacyPolicy/privacyPolicy'
 import About from '../components/AboutUs/AboutUs'
 import Form from '../components/Form/Form'
+import ContactUs from '../components/ContactUs/contactUs'
 import {ProfileProvider} from '../profileContext'
+import {HOME_ROUTE, 
+        DASHBOARD_ROUTE, 
+        NOT_FOUND_ROUTE, 
+        PRIVACYPOLICY_ROUTE, 
+        ABOUT_ROUTE, 
+        FORM_ROUTE,
+        CONTACTUS_ROUTE } from './routes'
 
 
 export default function Container() {
@@ -17,12 +25,13 @@ export default function Container() {
             <ProfileProvider>
                 <Navbar />
                     <Switch>
-                    <Route exact path="/" component={LandingPage} />
-                    <Route path="/dashboard" component={Dashboard} />
-                    <Route path="/PrivacyPolicy" component={PrivacyPolicy} />
-                    <Route path="/about" component={About} />
-                    <Route path="/form" component={Form} />
-                    <Route path="/:404" component={null} />
+                    <Route exact path={HOME_ROUTE} component={LandingPage} />
+                    <Route path={DASHBOARD_ROUTE} component={Dashboard} />
+                    <Route path={PRIVACYPOLICY_ROUTE} component={PrivacyPolicy} />
+                    <Route path={ABOUT_ROUTE} component={About} />
+                    <Route path={CONTACTUS_ROUTE} component={ContactUs} />
+                    <Route path={FORM_ROUTE} component={Form} />
+                    <Route path={NOT_FOUND_ROUTE} component={null} />
                     </Switch>
                 <Footer />
             </ProfileProvider>
