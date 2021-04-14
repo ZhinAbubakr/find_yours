0e400027af5ac454fbe342e5d04117894f33ad84
Murtaja Adnan <murtaja07805@gmail.com>
2021-04-14 15:25:45 +0300

    Refactor: change the posts in postssection to the same posts as in dashboard

--- src/components/PostsSection/PostsSection.jsx
+++ src/components/PostsSection/PostsSection.jsx
@@ -1,95 +1,73 @@
 import React, { useEffect, useState } from "react"
-import {db} from '../../firebase'
+import { db } from "../../firebase"
 import useStyles from "./style"
 import { ThemeProvider } from "@material-ui/styles"
 import { theme } from "./style"
-import {
-	Typography,
-	Button,
-	Grid,
-	CssBaseline,
-	Card,
-	CardActions,
-	CardContent,
-	Container,
-} from "@material-ui/core"
+import { Typography, Button, Grid, CssBaseline, Box } from "@material-ui/core"
+import Post from "../Dashboard/Post/Post"
 
 export default function Posts() {
-    const classes = useStyles();
-    const [posts,setPosts] =useState([])
+	const classes = useStyles()
+	const [posts, setPosts] = useState([])
+	const [postsNum, setPostsNum] = useState(10)
+	const [showMoreBtn, setShowMoreBtn] = useState(true)
 
-    useEffect(() => {
-        db.collection("posts").onSnapshot(snapshot => {
-            setPosts(snapshot.docs.map(doc => doc.data()))
-        })
-    }, [])
+	useEffect(() => {
+		const res = []
+		db.collection("posts")
+			.limit(postsNum)
+			.get()
+			.then((snapshot) => {
+				snapshot.docs.map((doc) => res.push(doc.data()))
+				setPosts(res)
+			})
+		db.collection("posts")
+			.get()
+			.then((snap) => setShowMoreBtn(snap.size > postsNum))
+	}, [postsNum])
 
-
-
-    return (
-        <React.Fragment>
-            <CssBaseline />
-            <main className={classes.heroContent}>
-                <div>
-                    <Container maxWidth="sm">
-                        <ThemeProvider theme={theme}>
-                            <Typography className={classes.heroTypography} component="h1" variant="h4" gutterBottom>
-                                Featured Lost Items
-                            </Typography>
-                            <Typography className={classes.heroTypography2} variant="h6" paragraph
-                            >
-                            View our recently featured Lost and Found property entries
-                            </Typography>
-                        </ThemeProvider>
-                        <div>
-                        <Grid>
-                            <Grid item xs={12}>
-                                <Container align="center" maxWidth="lg"> 
-                                 
-                                </Container>
-                            </Grid>
-                        </Grid></div>
-                    </Container>
-                </div>
-
-                <Container className={classes.cardGrid} maxWidth="lg">
-                    <Grid container spacing={3}>
-                        {posts.map((data,index)=>(
-                            <Grid item xs={12} sm={6} md={3} key={index} >
-                                <Card className={classes.card}>
-                                    <CardContent className={classes.cardContent}>
-                                        <ThemeProvider theme={theme}>
-                                            <Typography gutterBottom variant="h5" component="h2" maxLen={10} className={classes.headline} >
-                                                {data.body.substring(0,50)}
-                                            </Typography>
-                                            <Typography component="p" variant="body2" style={{
-                                                marginTop:"0.8rem"
-                                            }} >
-                                                {data.province}
-                                            </Typography>
-                                        </ThemeProvider>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button className={classes.btn} size="small" variant="outlined">
-                                            Contact
-                                        </Button>
-                                    </CardActions>
-                                </Card>
-                            </Grid>
-                        ))}
-                        <Grid item xs={12}>
-                            <Container align="center">
-                                <ThemeProvider theme={theme}>
-                                    <Button variant="contained" color="primary" className={classes.btn2}>
-                                        View More Lost Items
-                                    </Button>
-                                </ThemeProvider>
-                            </Container>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </main>
-        </React.Fragment>
-    );
+	return (
+		<div>
+			<CssBaseline />
+			<Box pb={2} pt={4}>
+				<ThemeProvider theme={theme}>
+					<Typography
+						className={classes.heroTypography}
+						component="h1"
+						variant="h4"
+						gutterBottom>
+						Featured Lost Items
+					</Typography>
+					<Typography
+						className={classes.heroTypography2}
+						variant="h6"
+						paragraph>
+						View our recently featured Lost and Found property entries
+					</Typography>
+				</ThemeProvider>
+			</Box>
+			<div className={classes.posts}>
+				{posts ? (
+					<>
+						{posts.map((post, i) => (
+							<Post key={i} post={post} />
+						))}
+						{showMoreBtn && (
+							<Grid container item justify="center">
+								<Box pb={2}>
+									<Button
+										onClick={() => setPostsNum(postsNum + 10)}
+										className="showMoreBtn">
+										show more
+									</Button>
+								</Box>
+							</Grid>
+						)}
+					</>
+				) : (
+					<CircularProgress />
+				)}
+			</div>
+		</div>
+	)
 }
-
