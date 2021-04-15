e6a3e48222eaa1a6142ead54d34529e17c5424a2
Othman Shareef <othmanosx@gmail.com>
2021-04-15 13:46:36 +0300

    Feat: add post to router

--- src/containers/Container.jsx
+++ src/containers/Container.jsx
@@ -1,42 +1,45 @@
-import React from "react"
-import Navbar from "../components/Navbar/Navbar"
-import Footer from "../components/Footer/Footer"
-import LandingPage from "./LandingPage"
-import Dashboard from "../components/Dashboard/Dashboard"
-import { BrowserRouter, Switch, Route } from "react-router-dom"
-import PrivacyPolicy from "../components/PrivacyPolicy/privacyPolicy"
-import About from "../components/AboutUs/AboutUs"
-import Form from "../components/Form/Form"
-import ContactUs from "./CountactUs"
-import { ProfileProvider } from "../profileContext"
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import LandingPage from "./LandingPage";
+import Dashboard from "../components/Dashboard/Dashboard";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import PrivacyPolicy from "../components/PrivacyPolicy/privacyPolicy";
+import About from "../components/AboutUs/AboutUs";
+import Form from "../components/Form/Form";
+import ContactUs from "./CountactUs";
+import { ProfileProvider } from "../profileContext";
 import {
-	HOME_ROUTE,
-	DASHBOARD_ROUTE,
-	NOT_FOUND_ROUTE,
-	PRIVACYPOLICY_ROUTE,
-	ABOUT_ROUTE,
-	FORM_ROUTE,
-	CONTACTUS_ROUTE
-} from "./routes"
+  HOME_ROUTE,
+  DASHBOARD_ROUTE,
+  NOT_FOUND_ROUTE,
+  PRIVACYPOLICY_ROUTE,
+  ABOUT_ROUTE,
+  FORM_ROUTE,
+  CONTACTUS_ROUTE,
+  POST_ROUTE,
+} from "./routes";
+import ViewPost from "../components/ViewPost/ViewPost";
 
 export default function Container() {
-	return (
-		<div>
-			<BrowserRouter>
-				<ProfileProvider>
-					<Navbar />
-					<Switch>
-						<Route exact path={HOME_ROUTE} component={LandingPage} />
-						<Route path={DASHBOARD_ROUTE} component={Dashboard} />
-						<Route path={PRIVACYPOLICY_ROUTE} component={PrivacyPolicy} />
-						<Route path={ABOUT_ROUTE} component={About} />
-						<Route path={CONTACTUS_ROUTE} component={ContactUs} />
-						<Route path={FORM_ROUTE} component={Form} />
-						<Route path={NOT_FOUND_ROUTE} component={null} />
-					</Switch>
-					<Footer />
-				</ProfileProvider>
-			</BrowserRouter>
-		</div>
-	)
+  return (
+    <div>
+      <BrowserRouter>
+        <ProfileProvider>
+          <Navbar />
+          <Switch>
+            <Route exact path={POST_ROUTE} component={ViewPost} />
+            <Route exact path={HOME_ROUTE} component={LandingPage} />
+            <Route path={DASHBOARD_ROUTE} component={Dashboard} />
+            <Route path={PRIVACYPOLICY_ROUTE} component={PrivacyPolicy} />
+            <Route path={ABOUT_ROUTE} component={About} />
+            <Route path={CONTACTUS_ROUTE} component={ContactUs} />
+            <Route path={FORM_ROUTE} component={Form} />
+            <Route path={NOT_FOUND_ROUTE} component={null} />
+          </Switch>
+          <Footer />
+        </ProfileProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
