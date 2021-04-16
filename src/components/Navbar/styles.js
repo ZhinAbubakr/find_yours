eedd192b585742863bdc625abcdf2553fec46f40
Othman Shareef <othmanosx@gmail.com>
2021-04-17 00:12:34 +0300

    Style: style active nav buttons

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -20,9 +20,15 @@ const useStyles = makeStyles(() => ({
 		fontFamily: "Oswald, sans-serif",
 		fontWeight: 700,
 		size: "18px",
+	},
+	navlink:{
+		textDecoration: 'none',
+		color: 'white',
 		marginLeft: "38px",
+		padding: '7px 0px 11px 0px',
 		"&.active": {
-			background:'grey'
+			borderRadius: '6px',
+			border: '1.5px solid rgba(255,255,255,0.5)',
 		  }, // eslint-disable-line no-mixed-spaces-and-tabs
 	},
 	toolbar: {
