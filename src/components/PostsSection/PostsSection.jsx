dc930cbe9ed98db05db50feebd9ea220522e21ab
Murtaja Adnan <murtaja07805@gmail.com>
2021-04-17 09:10:50 +0300

    Fix: fix error in the postssection

--- src/components/PostsSection/PostsSection.jsx
+++ src/components/PostsSection/PostsSection.jsx
@@ -1,35 +1,39 @@
-import React, { useEffect, useState } from "react"
-import { db } from "../../firebase"
-import useStyles from "./style"
-import { ThemeProvider } from "@material-ui/styles"
-import { theme } from "./style"
-import { Typography, Button, Grid, CssBaseline, Box } from "@material-ui/core"
-import Post from "../Dashboard/Post/Post"
+import React, { useEffect, useState } from "react";
+import { db } from "../../firebase";
+import useStyles from "./style";
+import { ThemeProvider } from "@material-ui/styles";
+import { theme } from "./style";
+import { Typography, Button, Grid, CssBaseline, Box } from "@material-ui/core";
+import Post from "../Dashboard/Post/Post";
 
 export default function Posts() {
-	const classes = useStyles()
-	const [posts, setPosts] = useState([])
-	const [postsNum, setPostsNum] = useState(3)
-	const [showMoreBtn, setShowMoreBtn] = useState(true)
+	const classes = useStyles();
+	const [posts, setPosts] = useState([]);
+	const [postsNum, setPostsNum] = useState(3);
+	const [showMoreBtn, setShowMoreBtn] = useState(true);
 
 	useEffect(() => {
-		const res = []
-		db.collection("posts")
-			.limit(postsNum)
-			.get()
-			.then((snapshot) => {
-				snapshot.docs.map((doc) =>
-					res.push({
-						id: doc.id,
-						...doc.data()
-					})
-				)
-				setPosts(res)
-			})
-		db.collection("posts")
-			.get()
-			.then((snap) => setShowMoreBtn(snap.size > postsNum))
-	}, [postsNum])
+		const fetch = () => {
+			const res = [];
+			db.collection("posts")
+				.limit(postsNum)
+				.get()
+				.then((snapshot) => {
+					snapshot.docs.map((doc) =>
+						res.push({
+							id: doc.id,
+							...doc.data()
+						})
+					);
+					setPosts(res);
+				});
+			db.collection("posts")
+				.get()
+				.then((snap) => setShowMoreBtn(snap.size > postsNum));
+		};
+		fetch();
+		return fetch();
+	}, [postsNum]);
 
 	return (
 		<div>
@@ -74,5 +78,5 @@ export default function Posts() {
 				)}
 			</div>
 		</div>
-	)
+	);
 }
