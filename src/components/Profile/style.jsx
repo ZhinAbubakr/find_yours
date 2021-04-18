f8aafbe8bc44229a276c745567ba8c017729cd80
Othman Shareef <othmanosx@gmail.com>
2021-04-18 23:29:27 +0300

    Style: fix font in profile

--- src/components/Profile/style.jsx
+++ src/components/Profile/style.jsx
@@ -1,80 +1,80 @@
-import { makeStyles } from "@material-ui/core/styles";
-import { createMuiTheme } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles'
+import { createMuiTheme } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
-	media: {
-		width: 180,
-		height: 180,
-		borderRadius: "120px",
-		padding: "10px",
-		marginLeft: "20px"
-	},
-	container: {
-		marginTop: theme.spacing(12)
-	},
-	container1: {
-		marginLeft: theme.spacing(8),
-		marginBottom: theme.spacing(20)
-	},
-	container2: {
-		marginTop: theme.spacing(2)
-	},
-	divider: {
-		marginTop: theme.spacing(4),
-		marginBottom: theme.spacing(4),
-		marginRight: theme.spacing(14),
-		marginLeft: theme.spacing(12)
-	},
-	divider1: {
-		marginTop: theme.spacing(14),
-		marginBottom: theme.spacing(4),
-		marginRight: theme.spacing(14),
-		marginLeft: theme.spacing(12)
-	},
-	btn2: {
-		marginTop: theme.spacing(4),
-		marginBottom: theme.spacing(4),
-		marginRight: theme.spacing(10),
-		float: "right",
-		fontWeight: "bold"
-	},
-	btn3: {
-		marginBottom: theme.spacing(2),
-		marginRight: theme.spacing(10),
-		float: "right",
-		fontWeight: "bold"
-	},
-	headerTypo: {
-		marginBottom: theme.spacing(8),
-		marginLeft: theme.spacing(4)
-	},
-	links: {
-		textDecoration: "none"
-	},
-	button: {
-		fontWeight: "bold",
-		backgroundColor: "#3aafa9",
-		color: "white"
-	},
-	noPosts: {
-		color: "red",
-		textAlign: "center",
-		fontSize: "1rem"
-	}
-}));
+  media: {
+    width: 180,
+    height: 180,
+    borderRadius: '120px',
+    padding: '10px',
+    marginLeft: '20px',
+  },
+  container: {
+    marginTop: theme.spacing(12),
+  },
+  container1: {
+    marginLeft: theme.spacing(8),
+    marginBottom: theme.spacing(20),
+  },
+  container2: {
+    marginTop: theme.spacing(2),
+  },
+  divider: {
+    marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(4),
+    marginRight: theme.spacing(14),
+    marginLeft: theme.spacing(12),
+  },
+  divider1: {
+    marginTop: theme.spacing(14),
+    marginBottom: theme.spacing(4),
+    marginRight: theme.spacing(14),
+    marginLeft: theme.spacing(12),
+  },
+  btn2: {
+    marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(4),
+    marginRight: theme.spacing(10),
+    float: 'right',
+    fontWeight: 'bold',
+  },
+  btn3: {
+    marginBottom: theme.spacing(2),
+    marginRight: theme.spacing(10),
+    float: 'right',
+    fontWeight: 'bold',
+  },
+  headerTypo: {
+    marginBottom: theme.spacing(8),
+    marginLeft: theme.spacing(4),
+  },
+  links: {
+    textDecoration: 'none',
+  },
+  button: {
+    fontWeight: 'bold',
+    backgroundColor: '#3aafa9',
+    color: 'white',
+  },
+  noPosts: {
+    color: 'red',
+    textAlign: 'center',
+    fontSize: '1rem',
+  },
+}))
 
 export const theme = createMuiTheme({
-	typography: {
-		fontFamily: ["Poppins", "sans-serif"].join(",")
-	},
-	palette: {
-		primary: {
-			main: "#17252a"
-		},
-		secondary: {
-			main: "#3aafa9"
-		}
-	}
-});
+  typography: {
+    fontFamily: ['Cairo', 'Poppins', 'sans-serif'].join(','),
+  },
+  palette: {
+    primary: {
+      main: '#17252a',
+    },
+    secondary: {
+      main: '#3aafa9',
+    },
+  },
+})
 
-export default useStyles;
+export default useStyles
