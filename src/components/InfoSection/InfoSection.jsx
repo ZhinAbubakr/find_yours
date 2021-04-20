a7d34b273a5f8235fb94afe50fe32abc42bf2ef1
Your Name <captainravyar@gmail.com>
2021-04-20 17:01:33 +0300

    Feat:Translate InfoSection

--- src/components/InfoSection/InfoSection.jsx
+++ src/components/InfoSection/InfoSection.jsx
@@ -4,31 +4,33 @@ import useStyles from './style'
 import image1 from '../../images/1.svg'
 import image2 from '../../images/2.svg'
 import image3 from '../../images/3.svg'
+import { useTranslation } from 'react-i18next'
 
 
 
 
 const InfoSection = () => {
+        const { t } = useTranslation()
 	const classes = useStyles()
 
     return (
-        <div className={classes.root}>
+        <div className={classes.root} dir={t('infoSection.direction')}>
          <Grid container spacing={10} >
                 <Grid item xs={12} lg={4}>
                         <img src={image1} className={classes.image}/>
-                        <h3 className={classes.title}>Lorem ipsum dolor sit amet.</h3>
-                        <p className={classes.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel odio nisi. Quisque a congue ante. Maecenas non dolor pretium.</p>
+                        <h3 className={classes.title}>{t('infoSection.easyHead')}</h3>
+                        <p className={classes.description}>{t('infoSection.easyDesc')}</p>
                 </Grid>
                 <Grid item xs={12} lg={4}>
                 <img src={image2} className={classes.image}/>
-                <h3 className={classes.title}>Lorem ipsum dolor sit amet,</h3>
-                        <p className={classes.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel odio nisi. Quisque a congue ante. Maecenas non dolor pretium.</p>                
+                <h3 className={classes.title}>{t('infoSection.intelligenceHead')}</h3>
+                        <p className={classes.description}>{t('infoSection.intelligenceDesc')}</p>                
                 </Grid>
                 <Grid item xs={12} lg={4}>
                 <img src={image3} className={classes.image}/>
 
-                <h3 className={classes.title}>Lorem ipsum dolor sit amet.</h3>
-                        <p className={classes.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel odio nisi. Quisque a congue ante. Maecenas non dolor pretium.</p>                
+                <h3 className={classes.title}>{t('infoSection.OutReachHead')}.</h3>
+                        <p className={classes.description}>{t('infoSection.OutReachDesc')}</p>                
                 </Grid>
          </Grid>
         </div>
