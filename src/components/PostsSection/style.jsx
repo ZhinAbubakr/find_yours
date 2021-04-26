196440c6514a6ee61f74c71e6f6af6dbf154c2fa
zhin <zheen99abubakr@gmail.com>
2021-04-26 08:44:11 +0300

    Doc: update readme file

--- src/components/PostsSection/style.jsx
+++ src/components/PostsSection/style.jsx
@@ -1,39 +1,40 @@
-import { makeStyles } from "@material-ui/core/styles"
-import { createMuiTheme } from "@material-ui/core/styles"
+import { makeStyles } from '@material-ui/core/styles'
+import { createMuiTheme } from '@material-ui/core/styles'
 
 const useStyles = makeStyles({
-	posts: {
-		display: "flex",
-		flexWrap: "wrap",
-		justifyContent: "space-evenly"
-	},
-	heroTypography: {
-		fontWeight: "bold",
-		textAlign: "center",
-		color: "textPrimary"
-	},
-	heroTypography2: {
-		textAlign: "center",
-		color: "textPrimary"
-	},
-	showMoreBtn: {
-		color: "white !important",
-		background: "#3aafa9 !important"
-	}
+  posts: {
+    display: 'flex',
+    marginBottom: 35,
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly',
+  },
+  heroTypography: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'textPrimary',
+  },
+  heroTypography2: {
+    textAlign: 'center',
+    color: 'textPrimary',
+  },
+  showMoreBtn: {
+    color: 'white !important',
+    background: '#3aafa9 !important',
+  },
 })
 
 export const theme = createMuiTheme({
-	typography: {
-		fontFamily: ["Poppins", "sans-serif"].join(",")
-	},
-	palette: {
-		primary: {
-			main: "#17252a"
-		},
-		secondary: {
-			main: "#3aafa9"
-		}
-	}
+  typography: {
+    fontFamily: ['Poppins', 'sans-serif'].join(','),
+  },
+  palette: {
+    primary: {
+      main: '#17252a',
+    },
+    secondary: {
+      main: '#3aafa9',
+    },
+  },
 })
 
 export default useStyles
